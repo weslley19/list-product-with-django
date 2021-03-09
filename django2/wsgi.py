2c1a80ef620487f5d6313d4ff0e03f0665392b18
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-03-09 12:09:19 -0300

    fix: revert whitenoise

--- django2/wsgi.py
+++ django2/wsgi.py
@@ -17,4 +17,4 @@ from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
 
-application = Cling(MediaCling(get_wsgi_application()))
+application = get_wsgi_application()
